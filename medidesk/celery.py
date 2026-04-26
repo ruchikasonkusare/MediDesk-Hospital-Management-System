c0e6f0bd76fbb60e1f047dde364a28f019aee03b
ruchikasonkusare <sonkusareruchika8@gmail.com>
2026-04-26 23:34:59 +0530

    fixed crash + settings

--- medidesk/celery.py
+++ medidesk/celery.py
@@ -1,8 +1,8 @@
-import os
-from celery import Celery
+# import os
+# from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medidesk.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medidesk.settings')
 
-app = Celery('medidesk')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+# app = Celery('medidesk')
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.autodiscover_tasks()
--- medidesk/settings.py
+++ medidesk/settings.py
@@ -153,10 +153,10 @@ CORS_ALLOW_ALL_ORIGINS = True   # In production: set CORS_ALLOWED_ORIGINS
 # ── Celery + Redis ─────────────────────────────────────────────────────────────
 # CELERY_BROKER_URL = 'redis://localhost:6379/0'
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Kolkata'
+# CELERY_ACCEPT_CONTENT = ['json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TIMEZONE = 'Asia/Kolkata'
 
 # CELERY_BEAT_SCHEDULE = {
 #     'appointment-reminders-hourly': {
